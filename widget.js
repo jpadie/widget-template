1480cf5044abd6484088bf12a9a04fc752b368e7
Christoph Pech <christoph.pech@gmail.com>
2017-11-07 16:27:40 +0100

    uptate & version

--- widget.js
+++ widget.js
@@ -158,7 +158,7 @@ cpdefine("inline:com-chilipeppr-widget-grbl", ["chilipeppr_ready", "jquerycookie
         controller_units: null,
         status: "Offline",
         version: "",
-        widgetVersion: '2017-09-03h',
+        widgetVersion: '$$$widgetVersion$$$',
         q_count: 0,
         alarm: false,
         spindleSpeed: 'Off',
